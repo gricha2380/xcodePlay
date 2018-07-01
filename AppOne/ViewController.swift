b67de51eba2091db1a352a0a1237af4370787215
GRicha2380 <gregor.richardson@gmail.com>
2018-06-30 19:37:09 -0700

    Initial Commit

---
+++ AppOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppOne
+//
+//  Created by Gregor Richardson on 6/30/18.
+//  Copyright © 2018 GRicha2380. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
