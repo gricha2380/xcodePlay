14e102c855531c6222bda297457e2e9b0f1c1068
GRicha2380 <gregor.richardson@gmail.com>
2018-07-05 20:10:36 -0700

    beginning stackview app

---
+++ AppThree/AppThree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppThree
+//
+//  Created by Gregor Richardson on 7/5/18.
+//  Copyright © 2018 GRicha2380. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
